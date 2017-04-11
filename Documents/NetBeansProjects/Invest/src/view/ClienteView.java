7a9a9cb6ad545983e1350d4d6bde0c4a4540bc83
Junio <junio.araujo01@yahoo.com>
2017-04-11 14:42:07 -0300

    inclusão da classe InvestPoupanca

--- Documents/NetBeansProjects/Invest/src/view/ClienteView.java
+++ Documents/NetBeansProjects/Invest/src/view/ClienteView.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package invest;
+package view;
 
 /**
  *
  * @author Jose Junio
  */
-public class Invest {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ClienteView {
     
 }
