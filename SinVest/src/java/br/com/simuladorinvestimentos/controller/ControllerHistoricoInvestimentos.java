c0656c8b8015f254cac4df3d9c0438dc17e23cce
Junio <junio.araujo01@yahoo.com>
2017-06-13 20:38:04 -0300

    Revisão do Histórico de Investimentos.

--- SinVest/src/java/br/com/simuladorinvestimentos/controller/ControllerHistoricoInvestimentos.java
+++ SinVest/src/java/br/com/simuladorinvestimentos/controller/ControllerHistoricoInvestimentos.java
@@ -7,6 +7,8 @@ package br.com.simuladorinvestimentos.controller;
 
 import br.com.simuladorinvestimentos.model.Cliente;
 import br.com.simuladorinvestimentos.model.HistoricoInvestimentos;
+import br.com.simuladorinvestimentos.model.InvestPoupanca;
+import br.com.simuladorinvestimentos.model.Investimento;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -19,16 +21,41 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 @SessionScoped
 public class ControllerHistoricoInvestimentos {
+    
+    private Investimento investimento;
+    private HistoricoInvestimentos histInvestimento;
 
 
     public ControllerHistoricoInvestimentos() {
     }
 
-    public List<HistoricoInvestimentos> recuperaHistorico() {
+    public List<HistoricoInvestimentos> recuperaHistorico() {       
         Cliente clienteLogado = (Cliente) FacesContext.getCurrentInstance().
                 getExternalContext().getSessionMap().get("user");
         List<HistoricoInvestimentos> comparacoes = clienteLogado.getHistorico();
+        investimento = new InvestPoupanca();
+        histInvestimento = comparacoes.get(0);        
+        investimento.setValor(histInvestimento.getInvestimentos().get(0).getValor());
+        investimento.setPeriodo(histInvestimento.getInvestimentos().get(0).getPeriodo());
         return comparacoes;
     }
+
+    public Investimento getInvestimento() {
+        return investimento;
+    }
+
+    public void setInvestimento(Investimento investimento) {
+        this.investimento = investimento;
+    }
+
+    public HistoricoInvestimentos getHistInvestimento() {
+        return histInvestimento;
+    }
+
+    public void setHistInvestimento(HistoricoInvestimentos histInvestimento) {
+        this.histInvestimento = histInvestimento;
+    }
+    
+    
 
 }
