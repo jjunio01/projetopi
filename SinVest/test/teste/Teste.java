1ea8c328d5e6eda1e7a8bb7f0c1f6b90a6fcce53
Junio <junio.araujo01@yahoo.com>
2017-11-17 00:38:04 -0300

    Alterações no .gitignore

--- SinVest/test/teste/Teste.java
+++ SinVest/test/teste/Teste.java
@@ -7,31 +7,12 @@ import java.util.List;
 
 public class Teste {
 
-    // ai é só jogar o atributo na sessão e recuperá-lo quando quiser. //métodos auxiliares que podem te ajudar: public FacesContext getFacesContext() { return FacesContext.getCurrentInstance(); } public HttpSession getSession() { return (HttpSession) getFacesContext().getExternalContext().getSession(false); } public HttpServletRequest getRequestSession() { return (HttpServletRequest) getFacesContext().getExternalContext().getRequest(); } 
+    // ai é só jogar o atributo na sessão e recuperá-lo quando quiser. //métodos auxiliares que podem te ajudar: public FacesContext getFacesContext() { return FacesContext.getCurrentInstance(); } public HttpSession getSession() { return (HttpSession) getFacesContext().getExternalContext().getSession(false); } public HttpServletRequest getRequestSession() { return (HttpServletRequest) getFacesContext().getExternalContext().getRequest(); }
     public static void main(String[] args) throws ErroSistema, IOException {
-
-        
 
         List<Investimento> lista = null;
 
-        
         System.out.println(lista);
-        /*if (!(lista.isEmpty())) {
 
-            for (int i = 0; i < lista.size(); i++) {
-                if (lista.get(i) instanceof InvestPoupanca) {
-                    System.out.println("----------------Poup");
-                    System.out.println(lista.get(i));
-                } else if (lista.get(i) instanceof InvestCDB) {
-                    System.out.println("----------------CDB");
-                    System.out.println(lista.get(i));
-                } else if (lista.get(i) instanceof InvestLCI) {
-                    System.out.println("----------------LCI");
-                    System.out.println(lista.get(i));
-                }
-
-            }
-
-        }*/
     }
 }
